e3a24c97aae6d08407f2f109c9dd9b5ccf99f6cc
SUMANTA KUMAR DAS <48908320+SUMANTA5@users.noreply.github.com>
2021-11-25 22:43:05 +0530

    Ktor Student Server

--- src/data/table/StudentTable.kt
+++ src/data/table/StudentTable.kt
@@ -5,9 +5,9 @@ import org.jetbrains.exposed.sql.Table
 
 object StudentTable: Table() {
 
-    val userId:Column<Int> = integer("userId").autoIncrement()
-    val name:Column<String> = varchar("name",512)
-    val age:Column<Int> = integer("age")
+    val userId = integer("userId").autoIncrement()
+    val name = varchar("name",512)
+    val age = integer("age")
 
     override val primaryKey: PrimaryKey = PrimaryKey(userId)
 
